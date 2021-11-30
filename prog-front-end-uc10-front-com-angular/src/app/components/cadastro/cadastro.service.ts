cec66c90984cb5804bb4ac16c74518664f7b1ef2
vinicius rosa <vinicius.a.rosa@unesp.br>
2021-11-30 19:45:48 -0300

    atividade presencial 02

--- prog-front-end-uc10-front-com-angular/src/app/components/cadastro/cadastro.service.ts
+++ prog-front-end-uc10-front-com-angular/src/app/components/cadastro/cadastro.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { Cadastro} from './cadastro.model';
 
 @Injectable({
@@ -10,36 +10,60 @@ import { Cadastro} from './cadastro.model';
 export class CadastroService {
 
   baseUrl = "http://localhost:3001/cadastro";
+
+
   constructor(private snackBar: MatSnackBar, 
               private http: HttpClient) { }
 
-  showMessege(msg: string): void {
+  showMessege(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg,'X',{
       duration: 6000,
-      verticalPosition: "bottom"
+      verticalPosition: "top",
+      panelClass: isError ? ['errorMsg'] : ['successMsg']
     })
   }
 
   create(cadastro : Cadastro):Observable<Cadastro>{
-    return this.http.post<Cadastro>(this.baseUrl,cadastro);
-  } 
+    return this.http.post<Cadastro>(this.baseUrl,cadastro).pipe(
+      map((obj) => obj),
+      catchError(e => this.errorMsg(e))
+    )
+  }
+  
+  errorMsg(e: any): Observable<any>{
+    console.log(e);
+    this.showMessege('erro', true);
+    return EMPTY
+  }
 
   read(): Observable<Cadastro[]>{
-    return this.http.get<Cadastro[]>(this.baseUrl)
+    return this.http.get<Cadastro[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError(e => this.errorMsg(e))
+    )
   }
 
   readById(id: number): Observable<Cadastro> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.get<Cadastro>(url)
+    return this.http.get<Cadastro>(url).pipe(
+      map((obj) => obj),
+      catchError(e => this.errorMsg(e))
+    )
   }
 
   updateCadastro(cadastro: Cadastro): Observable<Cadastro>{
     const url = `${this.baseUrl}/${cadastro.id}`
-    return this.http.put<Cadastro>(url, cadastro)
+    return this.http.put<Cadastro>(url, cadastro).pipe(
+      map((obj) => obj),
+      catchError(e => this.errorMsg(e))
+    )
   }
 
   deleteCadastro(id: number): Observable<Cadastro>{
     const url =`${this.baseUrl}/${id}`
-    return this.http.delete<Cadastro>(url)
+    return this.http.delete<Cadastro>(url).pipe(
+      map((obj) => obj),
+      catchError(e => this.errorMsg(e))
+    )
   }
 }
